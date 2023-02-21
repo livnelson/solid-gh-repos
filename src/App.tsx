57ed7115b953bb44fc1e733fe13915d410c59ca2
Liv Nelson <livnelson@me.com>
2023-02-21 13:42:44 -0800

    learning solidJS and TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { Route, Routes } from "solid-app-router"
+import { Component, createEffect, createSignal } from "solid-js"
+import Nav from "./components/Nav"
+import Home from "./pages/Home"
+import SavedRepos from "./pages/SavedRepos"
+
+const [username, setUsername] = createSignal("livnelson")
+const [repos, setRepos] = createSignal([])
+
+createEffect(async () => {
+  const res = await fetch(
+    `https://api.github.com/users/${username()}/repos?sort=created`
+  )
+  setRepos(await res.json())
+  // console.log(repos())
+})
+
+const App: Component = () => {
+
+  return (
+    <div class="container">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="savedrepos" element={<SavedRepos />} />
+      </Routes>
+    </div>
+  )
+}
+
+export { username, setUsername, repos }
+export default App
